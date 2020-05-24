bb1285f9cfe6833cb2302dcff8f0bad97074275d
mangob91 <yl3794@columbia.edu>
2020-05-24 16:39:57 -0500

    Tested out different dependency injections

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,7 +1,9 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
